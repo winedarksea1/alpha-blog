95b4416ff637ab0cf8ca11667a733c39801c584f
Rails Udemy Course <railsudemycourse@gmail.com>
2020-01-02 21:19:45 +0000

    Add partials and destroy action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -39,6 +39,12 @@ class ArticlesController < ApplicationController
     return 'show'
   end
 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "Article was successfully deleted"
+    redirect_to articles_path
+  end
   
   private 
     def article_params
