fa92e9cd03b81494106a3891d21a6fdb22ec454c
Rails Udemy Course <railsudemycourse@gmail.com>
2020-01-22 18:44:01 +0000

    Fix tests and update navbar with categories

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -7,11 +7,11 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
   end
 
   test "get new category form and create category" do
+    sign_in_as(@user, "password")
     get new_category_path  
     assert_template 'categories/new'
     
     assert_difference 'Category.count', 1 do
-      sign_in_as(@user, "password")
       post categories_path, params: { category: { name: "sports" } }
       follow_redirect!
     end
