2b4a50459fd7441dd13b0cb2610f4ba443c68578
Rails Udemy Course <railsudemycourse@gmail.com>
2020-01-22 18:26:59 +0000

    Add admin requirement to create categories

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -1,12 +1,17 @@
 require 'test_helper'
 
 class CreateCategoriesTest < ActionDispatch::IntegrationTest
+  def setup
+    # @category = Category.create(name: "sports")
+    @user = User.create(username: "john", email: "john@example.com", password: "password", admin: true)
+  end
 
   test "get new category form and create category" do
     get new_category_path  
     assert_template 'categories/new'
     
     assert_difference 'Category.count', 1 do
+      sign_in_as(@user, "password")
       post categories_path, params: { category: { name: "sports" } }
       follow_redirect!
     end
@@ -16,6 +21,7 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
   end
   
   test "invalid category submission results in failure" do
+    sign_in_as(@user, "password")
     get new_category_path  
     assert_template 'categories/new'
     
