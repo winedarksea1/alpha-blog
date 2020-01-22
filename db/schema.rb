53a456b03ba00d9453fcbb65c57f666c82f83e9e
Rails Udemy Course <railsudemycourse@gmail.com>
2020-01-22 20:32:04 +0000

    Add article and category assocation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200121182752) do
+ActiveRecord::Schema.define(version: 20200122201327) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.text     "description"
